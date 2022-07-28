403de1a5f40cfbc2d7fd62e3e307fef71a41e256
Marrielly Martines <marrichrys@gmail.com>
2022-07-28 16:43:15 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>{{ result }}</div>
+</template>
+<script setup>
+const { result, search } = useAlgoliaSearch('test_index') // pass your index name as param
+onMounted(async () => {
+  await search({ query: 'Samsung' })
+})
+</script>
